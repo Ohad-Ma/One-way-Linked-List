d8e12b4ae043cb296fdeefa8127eaf54ff8b99ee
Shrek <Shrek@DESKTOP-OH6PDBE>
2021-09-22 12:11:16 +0300

    Add project files.

---
+++ LinkedList/OneWayLinkedList.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace LinkedList
+{
+    /// <summary>
+    /// Simple C# one way linked list with basic methods:
+    /// - Print list
+    /// - Delete
+    /// - Contains
+    /// </summary>
+    public class OneWayLinkedList
+    {
+        // Head of the linked list
+        public Node head;
+        public class Node
+        {
+            // Node's variables
+            public int data;
+            public Node next;
+
+            //default constructor
+            public Node(int x)
+            {
+                this.data = x;
+                this.next = null;
+                
+            }
+        }
+
+        /// <summary>
+        /// Method that prints the One way LL 
+        /// </summary>
+        public void printList()
+        {
+            Node n = head;
+            while (n != null)
+            {
+                // print
+                Console.WriteLine(n.data + " ");
+                n = n.next;
+            }
+
+        }
+
+        /// <summary>
+        /// Check if value x is in the LL or not
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns>True or False</returns>
+        public bool contains(int x)
+        {
+            Node n = head;
+            while(n != null)
+            {
+                // Value equals
+                if (n.data == x)
+                    return true;
+                else
+                    n = n.next;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Delete value X within the list. 
+        /// If two (or more) are being found then delete the first found
+        /// </summary>
+        /// <param name="x"></param>
+        public void delete(int x)
+        {
+            Node n = head;
+            Node temp = n;
+            // Check if the head of the list equals to the given val
+            if (head.data == x)
+                head = n.next;
+            // Head isn't the value
+            else { 
+                while(n != null)
+                {
+                    // Check within the list and at the end of the list
+                    if(n.data == x)
+                    {
+                       temp.next = n.next;
+                       n = null;
+                    }
+                    else
+                    {
+                        temp = n;
+                        n = n.next;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/// <summary>
+/// Simple main to check
+/// </summary>
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OneWayLinkedList list = new OneWayLinkedList();
+            list.head = new OneWayLinkedList.Node(3);
+            list.head.next = new OneWayLinkedList.Node(4);
+            list.head.next.next = new OneWayLinkedList.Node(5);
+
+            // prints the list
+            list.printList();
+            // verify head is found
+            Console.WriteLine("does 3 is in the list? \n"+list.contains(3));
+            // deletes the head
+            list.delete(3);
+            // verify head's deletion
+            Console.WriteLine("does 3 is in the list? \n" + list.contains(3));
+
+
+        }
+    }
+}
